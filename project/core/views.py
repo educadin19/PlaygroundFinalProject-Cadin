dc657362af1c4bedc9a854753ba97c0eee4db491
Eduardo Cadin <eduardocadin@gmail.com>
2024-01-13 01:32:52 -0300

    Add herencia de templates

--- project/core/views.py
+++ project/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "core/base.html", {"nombre": "Pela-Fit"})
+    return render(request, "core/index.html", {"nombre": "Pela-Fit"})
 
