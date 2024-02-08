b91cdcf6bb45c79ffb5c5e1e46e6464e1d586387
Eduardo Cadin <eduardocadin@gmail.com>
2024-02-08 00:17:00 -0300

    casi listo

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,11 +1,16 @@
-from django.contrib import admin
 from django.urls import path, include
-from .views import index
-
-
+from .views import index, about
+from django.views.generic.base import TemplateView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import CustomLoginView, register
 
 urlpatterns = [
     path('', index, name="index"),
-    path('', include("cliente.urls"), name="cliente")
-
+    path('about', about, name="about"),
+    path('', include("cliente.urls"), name="cliente"),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(template_name="core/logout.html"), name="logout"),
+    path("register/", register, name="register"),
 ]
+
+
--- project/core/views.py
+++ project/core/views.py
@@ -1,5 +1,25 @@
+from django.contrib.auth.views import LoginView
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from .forms import *
 
 def index(request):
     return render(request, "core/index.html", {"nombre": "Pela-Fit"})
 
+def about(request):
+    return render(request, "core/about.html", {"nombre": "Pela-Fit"})
+
+class CustomLoginView(LoginView):
+    authentication_form = CustomAuthenticationForm
+    template_name = "core/login.html"
+
+
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, "core/index.html")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "core/register.html", {"form": form})
